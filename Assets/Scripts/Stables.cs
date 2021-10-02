dbff356828f97b48554f7d17ab6bde47ac6d8643
CFHM <hcdunn88@gmail.com>
2021-10-02 13:51:30 -0500

    added directional unlocks to stables

--- Assets/Scripts/Stables.cs
+++ Assets/Scripts/Stables.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Stables : MonoBehaviour
 {
+    enum StableType { Up, Down, Left, Right };
     //config params
     [SerializeField] float minDistBetweenStables = 3f;
     [SerializeField] Horse horse;
+    [SerializeField] Sprite[] stableTypes;
     
     
     //cached references
@@ -15,6 +17,9 @@ public class Stables : MonoBehaviour
     PlayerMove player;
     float maxSpawnDist;
     List<Stables> otherStables;
+    StableType myType;
+    Sprite mySprite;
+    StableType myUnlockKey;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +28,37 @@ public class Stables : MonoBehaviour
         stableSpawner = FindObjectOfType<StableSpawner>();
         player = FindObjectOfType<PlayerMove>();
         maxSpawnDist = stableSpawner.maxSpawnDist;
+        mySprite = GetComponent<SpriteRenderer>().sprite;
         PickDirection();
+        InitializeStableType();
+    }
+
+    private void InitializeStableType()
+    {
+        myType = (StableType)Random.Range(0, 4);
+        switch(myType)
+        {
+            case StableType.Up:
+                GetComponent<SpriteRenderer>().sprite = stableTypes[0];
+                myUnlockKey = myType;
+                break;
+            case StableType.Down:
+                GetComponent<SpriteRenderer>().sprite = stableTypes[1];
+                myUnlockKey = myType;
+                break;
+            case StableType.Left:
+                GetComponent<SpriteRenderer>().sprite = stableTypes[2];
+                myUnlockKey = myType;
+                if(transform.localScale.x <= 0) { myUnlockKey = StableType.Right; }
+                break;
+            case StableType.Right:
+                GetComponent<SpriteRenderer>().sprite = stableTypes[3];
+                myUnlockKey = myType;
+                if (transform.localScale.x <= 0) { myUnlockKey = StableType.Left; }
+                break;
+            default:
+                break;
+        }
     }
 
     private void PickDirection()
@@ -42,20 +77,60 @@ public class Stables : MonoBehaviour
         DespawnFromRange();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMove>())
+        {
+            switch(myUnlockKey)
+            {
+                case StableType.Up:
+                    if(Input.GetKey(KeyCode.UpArrow))
+                    {
+                        SpawnHorse();
+                    }
+                    break;
+                case StableType.Down:
+                    if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        SpawnHorse();
+                    }
+                    break;
+                case StableType.Left:
+                    if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        SpawnHorse();
+                    }
+                    break;
+                case StableType.Right:
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        SpawnHorse();
+                    }
+                    break;
+            }
+        }
+    }
+
+    
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerMove>())
         {
+            
             scoreKeeper.IncreaseScore();
             stableSpawner.DespawnStable();
             SpawnHorse();
             Destroy(gameObject);
         }
-    }
+    }*/
 
     private void SpawnHorse()
     {
         Instantiate(horse, transform.position, Quaternion.identity);
+        scoreKeeper.IncreaseScore();
+        stableSpawner.DespawnStable();
+        Destroy(gameObject);
     }
 
     public void DespawnFromRange()
